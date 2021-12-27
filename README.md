c6333f23d644e96f42f81d215887527888eea63d
GyeomFka <clazziquai01@gmail.com>
2021-12-27 18:12:22 +0900

    마무리

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 ### zero base 4주차 과제 제출
-#### 페이징 구현 
-- [baeldung.com](https://www.baeldung.com/spring-thymeleaf-pagination)
-- [thymeleaf.org](https://www.thymeleaf.org/doc/tutorials/2.1/usingthymeleaf.html)
+#### 게시판 페이징 구현
+
--- src/main/java/org/zerobase/paging/pageable/Paging.java
+++ src/main/java/org/zerobase/paging/pageable/Paging.java
@@ -4,14 +4,14 @@ import org.zerobase.paging.repository.PostRepository;
 
 public class Paging {
 
+    public static final int NOW_PAGE_NAVIGATION_BLOCK_COUNT = 10;
     public static final int NOW_PAGE_LIST_COUNT = 10;
-    public static final int NOW_VIEW_NAVIGATION_PAGE = 10;
 
-    private int nowPage; // 현재 페이지 ex)1page, 2page
-    private int viewNavPage = NOW_VIEW_NAVIGATION_PAGE;  //페이지 네비게이션 보주는 수
+    private int viewNavPage = NOW_PAGE_NAVIGATION_BLOCK_COUNT;  //한 화면의 페이지 네비게이션 블럭 수
     private int pageSize = NOW_PAGE_LIST_COUNT; //한 페이지에서 보여 줄 게시물 수
-    private int totalNavPage; //page블록의 개수
     private int totalArticle = PostRepository.getTotalCount(); //전체 게시글 수
+    private int nowPage; // 현재 페이지 ex)1page, 2page
+    private int totalNavPage; //page블록의 개수
     private int startRow; //각 페이지 첫 시작 게시글
     private int endRow; //각 페이지 마지막 게시글
 
